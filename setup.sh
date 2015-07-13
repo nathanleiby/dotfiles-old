fd15b7ee587a391503787464cdc6810a297fe8df
Nathan Leiby <nathan.leiby@clever.com>
2015-07-13 11:58:53 -0700

    setup.sh: creates dev-env

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+############################
+# Sets up a local dev env
+############################
+
+git clone git@github.com:gmarik/Vundle.vim.git vim/bundle/Vundle.vim
+./makesymlinks.sh
