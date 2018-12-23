601ba705ed43527653a07d8cf852c945f329362a
Nathan Leiby <nathan.leiby@clever.com>
2018-12-23 12:02:50 -0800

    cask installs: fzf bindings, spectacle

--- setup.sh
+++ setup.sh
@@ -10,6 +10,7 @@ brew install hub
 brew install ag
 brew install diff-so-fancy
 brew install fzf
+$(brew --prefix)/opt/fzf/install # install useful key bindings (CTRL-R) and fuzzy completion:
 brew install tree
 brew install gnu-sed --with-default-names
 
@@ -18,5 +19,8 @@ pip3 install --upgrade neovim
 
 # Install Tmux Package Manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# Window manager
+brew cask install spectacle
 
 ./makesymlinks.sh
